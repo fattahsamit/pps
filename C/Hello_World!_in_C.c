a1bbedc83da906fea0c0ffdbf20ae8f493dd1350
fattahsamit <fattahsamit@gmail.com>
2021-10-29 01:39:33 +0600

    Hello World

---
+++ C/Hello_World!_in_C.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[100];
+
+    //gets() easier way to input string
+    gets(s);
+    printf("Hello, World!\n");
+    //puts() easier way to output string
+    puts(s);
+
+    return 0;
+}
