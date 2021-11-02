5f2a32cdb6b6a46fc77b802de3afc32ce268b008
fattahsamit <fattahsamit@gmail.com>
2021-11-02 14:22:35 +0600

    fixed error

--- Python/Write_a_function.py
+++ Python/Write_a_function.py
@@ -1,10 +1,10 @@
 def is_leap(year):
-    if(year%4==0):
+    if(year%400==0):
         leap = True
-    elif(year%400==0):
+    elif(year%100==0):
         leap = False
     elif(year%4==0):
-        leap = True
+            leap = True
     else:
         leap = False
   
