a7bc048d98f862526d22b7be6e94f4178082614a
fattahsamit <fattahsamit@gmail.com>
2023-01-28 22:51:06 +0600

    data types

--- JS/10daysofJS/dataTypes.js
+++ JS/10daysofJS/dataTypes.js
@@ -1,11 +1,52 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/**
+ *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+ *   Print three lines:
+ *   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+ *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+ *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+ *
+ *	Parameter(s):
+ *   secondInteger - The string representation of an integer.
+ *   secondDecimal - The string representation of a floating-point number.
+ *   secondString - A string consisting of one or more space-separated words.
+ **/
 function performOperation(secondInteger, secondDecimal, secondString) {
   // Declare a variable named 'firstInteger' and initialize with integer value 4.
   const firstInteger = 4;
+  secondInteger = parseInt(secondInteger);
   let sumInteger = firstInteger + secondInteger;
   console.log(sumInteger);
 
   // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
   const firstDecimal = 4.0;
+  secondDecimal = parseFloat(secondDecimal);
   let sumDecimal = firstDecimal + secondDecimal;
   console.log(sumDecimal);
 
@@ -13,9 +54,10 @@ function performOperation(secondInteger, secondDecimal, secondString) {
   const firstString = "HackerRank ";
   let sumString = firstString + secondString;
   console.log(sumString);
-}
 
-let secondInteger = 12;
-let secondDecimal = 4.32;
-let secondString = "is the best place to learn and practice coding!";
-performOperation(secondInteger, secondDecimal, secondString);
+  // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+
+  // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+
+  // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+}
