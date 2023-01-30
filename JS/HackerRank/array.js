0ac46bea1422f1efd7b186e45028ae52e16b0087
fattahsamit <fattahsamit@gmail.com>
2023-01-31 00:43:08 +0600

    Second Largest number

--- JS/HackerRank/array.js
+++ JS/HackerRank/array.js
@@ -1,10 +1,20 @@
 function getSecondLargest(nums) {
-  let n = nums.length;
-  let a = 0;
+  let n = nums.length,
+    a = 0,
+    b = 0;
   for (let i = 0; i < n; i++) {
     if (nums[i] > a) {
       a = nums[i];
     }
-    return a;
   }
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > b) {
+      if (nums[i] == a) {
+        continue;
+      } else {
+        b = nums[i];
+      }
+    }
+  }
+  return b;
 }
