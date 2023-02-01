385128000e5f5491e60a6b589b50e7b61a16b36a
fattahsamit <fattahsamit@gmail.com>
2023-02-02 03:21:09 +0600

    secondLargest

---
+++ JS/Crackerz/secondLargest.js
@@ -0,0 +1,23 @@
+function secondLargest(numbers) {
+  let high2 = 0;
+  numbers.sort();
+  //   let high = 0;
+  //   for (let i = 0; i < numbers.length; i++) {
+  //     if (numbers[i] > high) {
+  //       high += numbers[i];
+  //     }
+  //   }
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] == numbers[numbers.length - 1]) {
+      continue;
+    }
+    if (numbers[i] > high2) {
+      high2 = numbers[i];
+    }
+  }
+  return high2;
+}
+
+const numbers = [10, 33, 5, 99, 6];
+const largest2 = secondLargest(numbers);
+console.log(largest2);
