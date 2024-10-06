734c8f6ae1e3b93cee13e6d27a655b6351f25d39
fattahsamit <fattahsamit@gmail.com>
2024-10-07 00:20:50 +0600

    Lists challenges

---
+++ Python/codedex/The Legend of Python/Lists/c5_dna.py
@@ -0,0 +1,16 @@
+# Challenge 5: DNA
+
+dna_sequence = ['GCT', 'AGC', 'AGG', 'TAA', 'ACT', 'CAT', 'TAT', 'CCC', 'ACG', 'GAA', 'ACC', 'GGA']
+
+item_to_find = 'CAT'
+item_found = False
+
+for i in dna_sequence:
+    if i == item_to_find:
+        item_found = True
+        break
+
+if item_found == True:
+    print("Item Found!")
+else:
+    print("Item not found.")
