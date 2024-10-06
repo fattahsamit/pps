532df6fcf0256430e68982cba05890a11df5d822
fattahsamit <fattahsamit@gmail.com>
2024-10-06 10:29:49 +0600

    Loop problems

---
+++ Python/codedex/The Legend of Python/Loops/19_detention.py
@@ -0,0 +1,4 @@
+# 19. Detention
+
+for i in range(100):
+    print('I will not use Snapchat in class')
---
+++ Python/codedex/The Legend of Python/Loops/20_99_bottles.py
@@ -0,0 +1,8 @@
+# 20. 99 Bottles
+
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
+
+print('No more bottles of beer on the wall')
