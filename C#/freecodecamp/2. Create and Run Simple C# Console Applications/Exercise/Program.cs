51f71f5d0b014f56c13469b4adcc3f55a19ab23c
fattahsamit <fattahsamit@gmail.com>
2024-11-04 20:33:40 +0600

    larger of two numbers

--- C#/freecodecamp/2. Create and Run Simple C# Console Applications/Exercise/Program.cs
+++ C#/freecodecamp/2. Create and Run Simple C# Console Applications/Exercise/Program.cs
@@ -10,3 +10,10 @@ int roll3 = dice.Next(50, 101);
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
+
+// // Code challenge: Implement a method of the Math class that returns the larger of two numbers
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
